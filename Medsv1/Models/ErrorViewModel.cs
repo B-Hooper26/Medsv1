fa4b6a7126c44614001b32318de9004e92dd014c
willy <willy@WILL_LAPTOP>
2023-11-14 10:23:45 +0000

    Add project files.

---
+++ Medsv1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medsv1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
